bb91c095c8eecda23dcfacca36edb4176e4ce32d
john.a.clark3 <john.a.clark3@gmail.com>
2022-02-25 10:47:48 -0700

    by ID
    have two different ways to hunt by id

--- server.js
+++ server.js
@@ -5,10 +5,33 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+//finds based on the title
+function filterByQuery(query, ideasArray) {
+  let filteredResults = ideasArray;
+  if (query.id) {
+    filteredResults = filteredResults.filter((idea) => idea.id === query.id);
+  }
+  return filteredResults;
+}
+// //finds based on the id number
+// function findById(id, ideasArray) {
+//   const result = ideasArray.filter((idea) => idea.id === id)[0];
+//   return result;
+// }
+
 app.get("/api/ideas", (req, res) => {
+  let results = ideas;
   console.log(req.query);
-  res.json(ideas);
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
 });
+
+// app.get("/api/ideas/:id", (req, res) => {
+//   const result = findById(req.params.id, ideas);
+//   res.json(result);
+// });
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
