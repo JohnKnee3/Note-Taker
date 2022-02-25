a2680bae61f96832ff9848c2ce39da6b18c5609e
john.a.clark3 <john.a.clark3@gmail.com>
2022-02-25 13:01:48 -0700

    adding to the note.json on the back end

--- server.js
+++ server.js
@@ -1,26 +1,51 @@
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
-const { ideas } = require("./db/ideas");
+const { notes } = require("./db/notes");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-//finds id using a query
-function filterByQuery(query, ideasArray) {
-  let filteredResults = ideasArray;
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
+//finds id using query
+function filterByQuery(query, notesArray) {
+  let filteredResults = notesArray;
   if (query.id) {
-    filteredResults = filteredResults.filter((idea) => idea.id === query.id);
+    filteredResults = filteredResults.filter((note) => note.id === query.id);
   }
   return filteredResults;
 }
-// //finds id based on parameters
-// function findById(id, ideasArray) {
-//   const result = ideasArray.filter((idea) => idea.id === id)[0];
-//   return result;
-// }
 
-app.get("/api/ideas", (req, res) => {
-  let results = ideas;
+//Creates a new note to add to the notes object
+function createNewNote(body, notesArray) {
+  const note = body;
+  notesArray.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, "./db/notes.json"),
+    JSON.stringify({ notes: notesArray }, null, 2)
+  );
+  return note;
+}
+
+//makes sure all of the Title and Text fields have information.
+function validateNote(note) {
+  if (!note.title || typeof note.title !== "string") {
+    return false;
+  }
+  if (!note.text || typeof note.text !== "string") {
+    return false;
+  }
+  return true;
+}
+
+//the GET that goes into the notes object and sends it to our filterByQuery function
+app.get("/api/notes", (req, res) => {
+  let results = notes;
   console.log(req.query);
   if (req.query) {
     results = filterByQuery(req.query, results);
@@ -28,12 +53,37 @@ app.get("/api/ideas", (req, res) => {
   res.json(results);
 });
 
-// app.get("/api/ideas/:id", (req, res) => {
-//   const result = findById(req.params.id, ideas);
-//   res.json(result);
-// });
+//the POST that talks to createNewNote to add the the notes object
+app.post("/api/notes", (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = notes.length.toString();
 
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateNote(req.body)) {
+    res.status(400).send("The note is not properly formatted.");
+  } else {
+    // add note to json file and notes array in this function
+    const note = createNewNote(req.body, notes);
+
+    res.json(note);
+  }
+});
+
+//Opens the server
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
-  console.log("http://localhost:3001/api/ideas");
+  console.log("http://localhost:3001/api/notes");
 });
+
+//the get code for using parameters instead of query
+
+// //finds id based on parameters
+// function findById(id, notesArray) {
+//   const result = notesArray.filter((note) => note.id === id)[0];
+//   return result;
+// }
+
+// app.get("/api/notes/:id", (req, res) => {
+//   const result = findById(req.params.id, notes);
+//   res.json(result);
+// });
